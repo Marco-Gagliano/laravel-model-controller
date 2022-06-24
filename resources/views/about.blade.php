1b21520bbf01527c653165c8d62a27f10f680a82
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-24 17:09:21 +0200

    creazione-model-controller-route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.main')
+
+@section('content')
+    <h1>ABOUT</h1>
+@endsection
+
+@section('title', 'About')
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@index')->name('home');
+
+Route::get('/about', 'PageController@about')->name('about');
+
+Route::get('/contact', 'PageController@contact')->name('contact');
+
+Route::get('/movie', 'MovieController@index')->name('movie');
